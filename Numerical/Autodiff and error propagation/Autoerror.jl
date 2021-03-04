f028f21b65fb87f7ebb03f40c1b3de2b4b2d6629
Kaonan Micadei <k.micadei@gmail.com>
2021-03-04 18:33:59 +0100

    Add julia module

--- Numerical/Autodiff and error propagation/Autoerror.jl
+++ Numerical/Autodiff and error propagation/Autoerror.jl
@@ -22,15 +22,17 @@ Base.promote_rule(::Type{Dual}, ::Type{<:Real}) = Dual
 
 Base.:+(f::Dual, g::Dual) = Dual(f.val + g.val, f.grad .+ g.grad)
 Base.:-(f::Dual, g::Dual) = Dual(f.val - g.val, f.grad .- g.grad)
+Base.:-(f::Dual) = 0 - f
 Base.:*(f::Dual, g::Dual) = Dual(f.val * g.val, f.grad .* g.val .+ f.val * g.grad)
 Base.:/(f::Dual, g::Dual) = Dual(f.val / g.val, (f.grad .* g.val .- f.val .* g.grad) ./ g.val^2)
-
-Base.:-(f::Dual) = 0 - f
 
 Base.sqrt(f::Dual) = Dual(sqrt(f.val), f.grad ./ sqrt(f.val))
 Base.log(f::Dual) = Dual(log(f.val), f.grad ./ f.val)
 Base.conj(f::Dual) = Dual(conj(f.val), conj.(f.grad))
 
-uncert(f::Dual, σ::Vector{<:Real}) = (val = f.val, err = √sum((f.grad .* σ).^2))
+function uncert(f::Dual, σ::Vector{<:Real})
+    x = f.grad .* σ
+    (val = f.val, err = √(x'x))
+end
 
 end
