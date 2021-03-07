e921e3e1be52f07c61c3fb50ed45d4b46f118562
Kaonan Micadei <k.micadei@gmail.com>
2021-03-07 21:09:43 +0100

    Minor corrections

--- Numerical/Autodiff and error propagation/Autoerror.jl/src/dualnumber.jl
+++ Numerical/Autodiff and error propagation/Autoerror.jl/src/dualnumber.jl
@@ -18,6 +18,7 @@ Base.promote_rule(::Type{Dual}, ::Type{<:Real}) = Dual
 
 Base.:+(f::Dual, g::Dual) = Dual(f.val + g.val, f.grad .+ g.grad)
 Base.:-(f::Dual, g::Dual) = Dual(f.val - g.val, f.grad .- g.grad)
-Base.:-(f::Dual) = 0 - f
 Base.:*(f::Dual, g::Dual) = Dual(f.val * g.val, f.grad .* g.val .+ f.val * g.grad)
 Base.:/(f::Dual, g::Dual) = Dual(f.val / g.val, (f.grad .* g.val .- f.val .* g.grad) ./ g.val^2)
+# also the negative of a dual number
+Base.:-(f::Dual) = Dual(-f.val, -f.grad)
